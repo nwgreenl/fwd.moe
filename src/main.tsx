66d6976f1c41dd32f002397417b2d94afc78ccee
Nick Greenlees <greenleesnick@gmail.com>
2026-07-14 15:36:47 -0700

    feat: add source info log

--- src/main.tsx
+++ src/main.tsx
@@ -3,4 +3,8 @@ import App from "./App";
 
 import "./index.css";
 
+console.info(
+  "hey (─‿‿─)~\n\nsource → https://github.com/nwgreenl/fwd.moe\nsupport → https://buymeacoffee.com/nwgreenl",
+);
+
 render(<App />, document.getElementById("app")!);
